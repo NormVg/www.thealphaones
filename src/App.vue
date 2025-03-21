fc8a3cdd1ce5ec1f64bf96f5cd258d8283519639
Vishnu Gupta <thenormvg@gmail.com>
2025-03-21 12:46:30 +0530

    -a
    done vercel analytis

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-
+import { Analytics } from '@vercel/analytics/vue';
 </script>
 
 <template>
-
+  <Analytics />g
 
   <RouterView />
 </template>
